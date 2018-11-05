a5da3df08e1f3901514c1607c79344729ac071b6
Matheus J <alvesmatheus103@gmail.com>
2018-11-05 00:16:08 -0200

    lucro

--- pages/ControleLucro.php
+++ pages/ControleLucro.php
@@ -1,9 +1,44 @@
 	<!-- Modal -->
-				  <div class="clear"></div>
-				  <h1 class="page-header">Controle de Lucro</h1>
-				  <div class="table-responsive">
+	<div class="clear"></div>
+	<h1 class="page-header">Controle de Lucro</h1>
+<!-- 				  <div class="table-responsive">
 						<iframe width="700" height="400" frameborder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=AECE3BFA79D316B%21387&authkey=%21AFr2uH23mkuRPjA&em=2&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True"></iframe>
 				</div>
 				<div id="paginacao" class="w100">
 					<ul class="pagination"></ul>
-				</div>
+				</div> -->
+<!-- 				<?php 
+				$sql = MySql::conectar()->prepare("
+					SELECT SUM(total) as receita, data from tb_pedido_finalizado
+					GROUP BY data
+					");
+				$sql->execute();
+				$sql = $sql->fetchAll();
+				foreach ($sql as $key => $value):
+					?>
+					<th><?php echo $value['receita'];?></th>
+				<?php endforeach;?> -->
+
+
+<div class="table-responsive">
+	<table class="table table-striped table-bordered table-hover" id="list-thumbs">
+		<thead>
+			<tr>
+				<th>Total</th>
+				<th>Data</th>
+			</tr>
+				<?php 
+				$historico = MySql::conectar()->prepare("
+					SELECT SUM(total) as total, data FROM tb_pedido_finalizado GROUP BY data
+					");
+				$historico->execute();
+				$historico = $historico->fetchAll();
+
+				foreach ($historico as $key => $value):
+					?>
+
+					<tr>
+						<td><?php echo $value['total'];?></td>
+						<td><?php echo $value['data'];?></td>
+						</tr>
+					<?php endforeach;?>
--- pages/ListaFuncionarios.php
+++ pages/ListaFuncionarios.php
@@ -1,13 +1,13 @@
 <?php 
-	$sql = MySql::conectar()->prepare("
+$sql = MySql::conectar()->prepare("
 	SELECT *
 	FROM tb_funcionario F
 	INNER JOIN tb_cargo C
 	ON F.id_cargo = C.idcargo
 	WHERE F.status != 0
 	");
-	$sql->execute();
-	$qtd = $sql->rowCount();
+$sql->execute();
+$qtd = $sql->rowCount();
 ?>
 <div style="width: 100%;">
 	<div class="left" style="width: 50%;float: left;">
@@ -19,99 +19,99 @@
 		<label for="sel1">Resultados por página:</label> 
 		<select class="form-control" id="sel1" onchange="pagination(this,'#list-thumbs','#pesquisaSlide')">
 			
-				<option value="5">5</option>
-				<?php if($qtd > 5):?>
-					<option value="10">10</option>
-				<?php endif;?>
-		
+			<option value="5">5</option>
+			<?php if($qtd > 5):?>
+				<option value="10">10</option>
+			<?php endif;?>
+			
 		</select>
 	</div>
 	<div class="clear"></div>
 </div>
 <h1 class="page-header">Lista de Funcionários</h1>
-			<div class="table-responsive">
-				<table class="table table-striped table-bordered table-hover" id="list-thumbs">
-			      	<thead>
-				        <tr>
-				          	<th>CPF</th>
-				          	<th>Nome</th>
-				          	<th>Cargo</th>
-				          	<th>Ação</th>
-				        </tr>
-			      	</thead>
-			  		<tbody>
-			  			<?php 
-			  				$funcionarios = MySql::conectar()->prepare("
-								SELECT *
-								FROM tb_funcionario F
-								INNER JOIN tb_cargo C
-								ON F.id_cargo = C.idcargo
-								WHERE F.status != 0
-			  				");
-			  				$funcionarios->execute();
-			  				$funcionarios = $funcionarios->fetchAll();
+<div class="table-responsive">
+	<table class="table table-striped table-bordered table-hover" id="list-thumbs">
+		<thead>
+			<tr>
+				<th>CPF</th>
+				<th>Nome</th>
+				<th>Cargo</th>
+				<th>Ação</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php 
+			$funcionarios = MySql::conectar()->prepare("
+				SELECT *
+				FROM tb_funcionario F
+				INNER JOIN tb_cargo C
+				ON F.id_cargo = C.idcargo
+				WHERE F.status != 0
+				");
+			$funcionarios->execute();
+			$funcionarios = $funcionarios->fetchAll();
 
-			  				foreach ($funcionarios as $key => $value):
-			  			?>
-			    		<tr>
-				          	<th><?php echo $value['cpf'];?></th>
-				          	<td><?php echo $value['nome'].' '.$value['sobrenome'];?></td>
-				          	<td><?php echo $value['cargo'];?></td>
-				          	<td>
-				          		<form method="post" style="display: inline-block;">
-				          			<input type="hidden" name="id" value="<?php echo $value['idfuncionario']; ?>"/>
+			foreach ($funcionarios as $key => $value):
+				?>
+				<tr>
+					<th><?php echo $value['cpf'];?></th>
+					<td><?php echo $value['nome'].' '.$value['sobrenome'];?></td>
+					<td><?php echo $value['cargo'];?></td>
+					<td>
+						<form method="post" style="display: inline-block;">
+							<input type="hidden" name="id" value="<?php echo $value['idfuncionario']; ?>"/>
 
-				          			<input type="hidden" name="acao" value="editarF">
-				          			<input type="hidden" name="formulario" value="funcionario">
+							<input type="hidden" name="acao" value="editarF">
+							<input type="hidden" name="formulario" value="funcionario">
 
-						            <button class="btn btn-warning btn-xs" title="Alterar" data-toggle="modal" data-target="#myModal" type="submit">
-					              		<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-						            </button>
-						        </form>
-						        <form method="post" style="display: inline-block;">
-				          			<input type="hidden" name="id" value="<?php echo $value['idfuncionario']; ?>"/>
+							<button class="btn btn-warning btn-xs" title="Alterar" data-toggle="modal" data-target="#myModal" type="submit">
+								<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+							</button>
+						</form>
+						<form method="post" style="display: inline-block;">
+							<input type="hidden" name="id" value="<?php echo $value['idfuncionario']; ?>"/>
 
-				          			<input type="hidden" name="acao" value="readDeleteF">
-				          			<input type="hidden" name="formulario" value="funcionario">
+							<input type="hidden" name="acao" value="readDeleteF">
+							<input type="hidden" name="formulario" value="funcionario">
 
-						            <button class="btn btn-danger btn-xs" title="Remover" type="submit" data-toggle="modal" data-target="#myModal" type="submit">
-						              	<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-						            </button>
-					            </form>
-			      			</td>
-			    		</tr>
-			    		<?php endforeach;?>
-				       	<tr class="nenhum-registro">
-							<td colspan="6" style="text-align: center;">Nenhum funcionário encontrado...</td>
-						</tr>
-			  		</tbody>
-				</table>
-			</div>
-			<div id="paginacao" class="w100">
-				<ul class="pagination"></ul>
-			</div>
-			<div class="carregar">
-				
-			</div>
-    	</div>
-	</div>
+							<button class="btn btn-danger btn-xs" title="Remover" type="submit" data-toggle="modal" data-target="#myModal" type="submit">
+								<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+							</button>
+						</form>
+					</td>
+				</tr>
+			<?php endforeach;?>
+			<tr class="nenhum-registro">
+				<td colspan="6" style="text-align: center;">Nenhum funcionário encontrado...</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+<div id="paginacao" class="w100">
+	<ul class="pagination"></ul>
+</div>
+<div class="carregar">
+	
+</div>
+</div>
+</div>
 </div>
 
 <!-- Modal -->
 <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-    <!-- Modal content-->
-	    <div class="modal-content">
-	        <div class="modal-header">
-	          <button type="button" class="close" data-dismiss="modal">&times;</button>
-	          <h4 class="modal-title"></h4>
-	        </div>
-	        <div class="modal-body">
-	        	
-	        </div>
-	        <div class="modal-footer">
-	          <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
-	        </div>
+	<div class="modal-dialog">
+		<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title"></h4>
+			</div>
+			<div class="modal-body">
+				
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+			</div>
 		</div>    
 	</div>
 </div>
